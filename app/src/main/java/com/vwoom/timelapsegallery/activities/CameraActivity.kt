b2912476768eecc89e41af7209c87b0160c90353
Christopher Thompson <ChristopherErikThompson@Gmail.com>
2019-09-24 10:44:42 -0700

    refactor: configure kotlin

---
+++ app/src/main/java/com/vwoom/timelapsegallery/activities/CameraActivity.kt
@@ -0,0 +1,13 @@
+package com.vwoom.timelapsegallery.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vwoom.timelapsegallery.R
+
+class CameraActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+    }
+}
