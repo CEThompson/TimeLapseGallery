d9ea091cc1219ba446606217a5fa106329525fee
Christopher Thompson <ChristopherErikThompson@Gmail.com>
2019-10-02 15:09:37 -0700

    refactor: update notification worker on shared preference change

--- app/src/main/java/com/vwoom/timelapsegallery/activities/SettingsActivity.kt
+++ app/src/main/java/com/vwoom/timelapsegallery/activities/SettingsActivity.kt
@@ -1,13 +1,19 @@
 package com.vwoom.timelapsegallery.activities
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
 import com.vwoom.timelapsegallery.R
+import com.vwoom.timelapsegallery.notification.NotificationUtils
 
 class SettingsActivity : AppCompatActivity() {
+
+    lateinit var prefs: SharedPreferences
+    lateinit var prefListener: SharedPreferences.OnSharedPreferenceChangeListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +30,30 @@ class SettingsActivity : AppCompatActivity() {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
 
             // TODO (update) implement project import button here
-            val syncButton: Preference? =  findPreference(getString(R.string.key_sync))
+            //val syncButton: Preference? =  findPreference(getString(R.string.key_sync))
+
+            // Listen for changes to shared preferences and update notification worker on change
+            (activity as SettingsActivity).prefs = PreferenceManager.getDefaultSharedPreferences(activity);
+            (activity as SettingsActivity).prefListener = SharedPreferences.OnSharedPreferenceChangeListener { prefs, key ->
+                Log.d("settings activity", "Notification listener activitating for key = $key")
+                if (key.equals(this.getString(R.string.key_notifications_enabled)))
+                    NotificationUtils.scheduleNotificationWorker(activity);
+            }
+
+        }
+
+        override fun onResume() {
+            super.onResume()
+            (activity as SettingsActivity).prefs.registerOnSharedPreferenceChangeListener(
+                    (activity as SettingsActivity).prefListener
+            )
+        }
+
+        override fun onPause() {
+            super.onPause()
+            (activity as SettingsActivity).prefs.unregisterOnSharedPreferenceChangeListener(
+                    (activity as SettingsActivity).prefListener
+            )
         }
     }
 }
