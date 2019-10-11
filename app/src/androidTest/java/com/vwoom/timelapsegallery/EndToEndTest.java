e3a5775ecc4e7413d93902615f7cb22e97082a98
Christopher Thompson <ChristopherErikThompson@Gmail.com>
2019-10-11 10:11:25 -0700

    chore: laying out test

---
+++ app/src/androidTest/java/com/vwoom/timelapsegallery/EndToEndTest.java
@@ -0,0 +1,27 @@
+package com.vwoom.timelapsegallery;
+
+public class EndToEndTest {
+
+    // TODO click new project fab
+
+    // TODO enter project name
+
+    // TODO click add photo fab
+
+    // TODO take photo
+
+    // TODO submit new project
+
+    // TODO click submitted project
+
+    // TODO click add photo fab
+
+    // TODO click add photo fab
+
+    // TODO take photo
+
+    // TODO submit new photo
+
+    // TODO verify new photo submitted
+    
+}
