bd020bf574d15280265474ed06375c3e44589ef3
Christopher Thompson <ChristopherErikThompson@Gmail.com>
2019-10-14 17:25:05 -0700

    style: clean up and comment test

--- app/src/androidTest/java/com/vwoom/timelapsegallery/EndToEndTest.java
+++ app/src/androidTest/java/com/vwoom/timelapsegallery/EndToEndTest.java
@@ -3,31 +3,20 @@ package com.vwoom.timelapsegallery;
 import android.app.Activity;
 import android.app.Instrumentation;
 import android.content.Context;
-import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.util.Log;
 
-import androidx.test.espresso.NoMatchingViewException;
 import androidx.test.espresso.contrib.RecyclerViewActions;
-import androidx.test.espresso.core.internal.deps.guava.collect.Iterables;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.filters.LargeTest;
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.lifecycle.ActivityLifecycleMonitorRegistry;
 import androidx.test.runner.lifecycle.Stage;
 
 import com.vwoom.timelapsegallery.activities.AddPhotoActivity;
 import com.vwoom.timelapsegallery.activities.MainActivity;
 import com.vwoom.timelapsegallery.activities.NewProjectActivity;
-import com.vwoom.timelapsegallery.database.TimeLapseDatabase;
-import com.vwoom.timelapsegallery.database.entry.PhotoEntry;
-import com.vwoom.timelapsegallery.database.entry.ProjectEntry;
 import com.vwoom.timelapsegallery.utils.FileUtils;
 
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -44,17 +33,19 @@ import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.Intents.intending;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.anyIntent;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static androidx.test.espresso.matcher.RootMatchers.isDialog;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 
-// TODO put together end to end test
+/*
+* This test adds a new project with a test photo bypassing the implicit camera intent.
+* A new photo is added to the project and the project is deleted.
+* This represents the overall basic workflow of the application.
+ */
 @LargeTest
 public class EndToEndTest {
 
@@ -91,29 +82,6 @@ public class EndToEndTest {
         // Submit the new project
         onView(withId(R.id.submit_new_project_fab)).perform(click());
 
-        /* Submit a second project*/
-        /*
-        onView(withId(R.id.add_project_FAB)).perform(click());
-
-        // Enter a project name
-        onView(withId(R.id.project_name_edit_text)).perform(replaceText("horizontalTestProject"));
-
-        // Create a test photo file
-        temp = null;
-        try {temp = FileUtils.createTemporaryImageFile(mContext);}
-        catch (IOException e){
-            Log.d(TAG, "temp file creation failed");
-        }
-        // Assert the file was created then copy test image to it
-        Assert.assertNotNull(temp);
-        writeDrawableToTempFile(temp, R.drawable.htest);
-
-        currentActivity=getActivityInstance();
-        ((NewProjectActivity)currentActivity).setmTemporaryPhotoPath(temp.getAbsolutePath());
-
-        // Submit the new project
-        onView(withId(R.id.submit_new_project_fab)).perform(click());
-        */
         // Click the first project
         // Assumes project list is empty
         onView(withId(R.id.projects_recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
@@ -133,27 +101,24 @@ public class EndToEndTest {
         // Assert the file was created then copy test image to it
         Assert.assertNotNull(h);
 
-        // TODO intent stub here...
+        // Set up intent stub for clicking on take photo fab
         Instrumentation.ActivityResult result = new Instrumentation.ActivityResult(Activity.RESULT_OK, null);
-
         intending(anyIntent()).respondWith(result);
 
+        // Click the fab to take a photo
         onView(withId(R.id.take_photo_fab)).perform(click());
+
+        // Overwrite the temporary photo path
+        // Note that in test the UI will not display the photo but the path should work for test submission
         writeDrawableToTempFile(h, R.drawable.vtest);
         ((AddPhotoActivity)currentActivity).setmTemporaryPhotoPath(h.getAbsolutePath());
 
-        //intended(anyIntent());
-        //try {Thread.sleep(3000);} catch(InterruptedException e){
-            // do nothing}
-        //}
-
-        // Submit the temporary photo
+        // Submit the photo
         onView(withId(R.id.submit_photo_fab)).perform(click());
 
+        // Click the option to delete the project
         openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
-
         onView(withText(R.string.delete_project)).perform(click());
-
         onView(withText(android.R.string.yes))
                 .inRoot(isDialog())
                 .check(matches(isDisplayed()))
@@ -162,30 +127,9 @@ public class EndToEndTest {
                 .inRoot(isDialog())
                 .check(matches(isDisplayed()))
                 .perform(click());
-
-        /*
-        onView(withId(R.id.projects_recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
-
-        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
-
-        onView(withText(R.string.delete_project)).perform(click());
-
-        onView(withText(android.R.string.yes))
-                .inRoot(isDialog())
-                .check(matches(isDisplayed()))
-                .perform(click());
-        onView(withText(android.R.string.yes))
-                .inRoot(isDialog())
-                .check(matches(isDisplayed()))
-                .perform(click());
-
-        try {onView(withText(R.string.welcome)).perform(click());}
-        catch (NoMatchingViewException e){
-        }
-        */
-
     }
 
+    /* Copies a drawable to a file */
     private void writeDrawableToTempFile(File destinationFile, int testImage){
         try {
             InputStream inputStream = getInstrumentation()
@@ -204,6 +148,7 @@ public class EndToEndTest {
         }
     }
 
+    /* Gets the current activity instance for espresso */
     private Activity getActivityInstance(){
         final Activity[] currentActivity = {null};
 
