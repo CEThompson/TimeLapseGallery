d3bb47a27eaad9ad865a0ef73875c4132d3b3693
Christopher Thompson <ChristopherErikThompson@Gmail.com>
2019-10-20 13:38:51 -0700

    chore: add todo

--- app/src/main/java/com/vwoom/timelapsegallery/activities/DetailsActivity.java
+++ app/src/main/java/com/vwoom/timelapsegallery/activities/DetailsActivity.java
@@ -566,6 +566,7 @@ public class DetailsActivity extends AppCompatActivity implements DetailsAdapter
                 .into(mNextPhotoImageView);
     }
 
+    // TODO lock down image playing
     /* Loads the set of images concurrently */
     private void playSetOfImages(){
         // Lazy Initialize handler
