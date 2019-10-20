77e3c893b0cd212f298645fbdf9facbb9d947123
Christopher Thompson <ChristopherErikThompson@Gmail.com>
2019-10-19 17:24:06 -0700

    refactor: Restructured settings activity to be an activity with two child fragments. The settings fragment does not retain instance state, retaining instance seemed to cause preference categories to display incorrectly. The async task is now wrapped in a fragment not visible in the ui

--- app/src/main/java/com/vwoom/timelapsegallery/activities/SettingsActivity.kt
+++ app/src/main/java/com/vwoom/timelapsegallery/activities/SettingsActivity.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.os.AsyncTask
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
 import android.view.View
 import android.view.Window
@@ -24,47 +23,154 @@ import com.vwoom.timelapsegallery.utils.ProjectUtils
 
 const val TAG = "Settings Tracker"
 
-class SettingsActivity : AppCompatActivity(), SettingsFragment.TaskCallbacks {
+class SettingsActivity : AppCompatActivity(), TaskFragment.TaskCallbacks, SettingsFragment.TaskCallbacks {
 
-    var settingsFragment: Fragment? = null
-    var syncDialog: Dialog? = null
+    // Fragments
+    var mSettingsFragment: Fragment? = null
+    var mTaskFragment: Fragment? = null
 
-    var syncing: Boolean? = null
+    // Dialogs
+    var mSyncDialog: Dialog? = null
+    var mFileModDialog: Dialog? = null
+    var mVerifySyncDialog: Dialog? = null
+
+    // State
+    var mSyncing: Boolean? = null
+    var mShowingFileModDialog: Boolean? = null
+    var mShowingVerifySyncDialog: Boolean? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d(TAG, "onCreate")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
 
-        settingsFragment = supportFragmentManager.findFragmentByTag("settings_fragment")
-        if (settingsFragment == null) settingsFragment = SettingsFragment()
+        mSettingsFragment = supportFragmentManager.findFragmentByTag("settings_fragment")
+        mTaskFragment = supportFragmentManager.findFragmentByTag("task_fragment")
+
+        if (mSettingsFragment == null) mSettingsFragment = SettingsFragment()
+        if (mTaskFragment == null) mTaskFragment = TaskFragment()
 
         supportFragmentManager
                 .beginTransaction()
-                .replace(R.id.settings, settingsFragment as PreferenceFragmentCompat, "settings_fragment")
+                .replace(R.id.settings, mSettingsFragment as PreferenceFragmentCompat, "settings_fragment")
+                .commit()
+
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.task_container, mTaskFragment as TaskFragment, "task_fragment")
                 .commit()
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        // Set up dialogs
         createSyncDialog()
+        createVerifyProjectImportDialog()
+        createManualFileModificationDialog()
 
         // Restore instance state of sync dialog
         if (savedInstanceState != null) {
-            syncing = savedInstanceState.getBoolean("syncing", false)
-            if (syncing!!) showSyncDialog()
-            Log.d(TAG, "restored instance state, syncing is $syncing")
+            mSyncing = savedInstanceState.getBoolean("mSyncing", false)
+            mShowingFileModDialog = savedInstanceState.getBoolean("mShowingFileModDialog", false)
+            mShowingVerifySyncDialog = savedInstanceState.getBoolean("mShowingVerifySyncDialog", false)
+
+            if (mSyncing!!) showSyncDialog()
+            else if (mShowingFileModDialog!!) showFileModificationDialog()
+            else if (mShowingVerifySyncDialog!!) showVerifyProjectImportDialog()
+
+            Log.d(TAG, "restored instance state, mSyncing is $mSyncing, mShowingFileModDialog is" +
+                    " $mShowingFileModDialog, mShowingVerifySync is $mShowingVerifySyncDialog")
         }
     }
 
+    fun createSyncDialog(){
+        Log.d(TAG, "creating sync dialog")
+        mSyncDialog = Dialog(this)
+        mSyncDialog?.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        // Set the dialog
+        mSyncDialog?.setCancelable(false)
+        mSyncDialog?.setContentView(R.layout.sync_dialog)
+
+        // Set up the button
+        val button = mSyncDialog?.findViewById(R.id.sync_verification_button) as androidx.appcompat.widget.AppCompatButton
+        button.setOnClickListener {
+            mSyncDialog?.dismiss()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.d(TAG, "saving instance state")
+        super.onSaveInstanceState(outState)
+        if (mSyncing == true) outState.putBoolean("mSyncing", true)
+        if (mShowingFileModDialog == true) outState.putBoolean("mShowingFileModDialog", true)
+        if (mShowingVerifySyncDialog == true) outState.putBoolean("mShowingVerifySyncDialog", true)
+    }
+
+    /*
+    override fun onPause() {
+        super.onPause()
+        if (mSyncDialog != null){
+            if (mSyncDialog?.isShowing!!)
+                mSyncDialog?.dismiss()
+        }
+    }
+    */
+
+    // Shows a dialog to give progress feedback on synchronization
+    fun showSyncDialog(){
+        Log.d(TAG, "showing sync dialog")
+        // Set the response
+        val responseView = mSyncDialog?.findViewById(R.id.sync_response) as TextView
+        responseView.setText(this.getString(R.string.executing_sync_notification))
+        val progress = mSyncDialog?.findViewById(R.id.sync_progress) as ProgressBar
+        /// Set the image feedback
+        val imageFeedback = mSyncDialog?.findViewById(R.id.sync_feedback_image) as ImageView
+        val button = mSyncDialog?.findViewById(R.id.sync_verification_button) as androidx.appcompat.widget.AppCompatButton
+
+        // Show the updated views
+        progress.visibility = View.VISIBLE
+        imageFeedback.visibility = View.INVISIBLE
+        button.visibility = View.GONE
+        // Show the dialog
+        mSyncDialog?.show()
+    }
+
+    // Updates the dialog showing progress on synchronization
+    fun updateSyncDialog(response: String){
+        Log.d(TAG, "updating sync dialog")
+        val success = (response.equals(getString(R.string.valid_file_structure)))
+
+        // Set the response
+        val responseView = mSyncDialog?.findViewById(R.id.sync_response) as TextView
+        responseView.setText(response)
+
+        val progress = mSyncDialog?.findViewById(R.id.sync_progress) as ProgressBar
+
+        /// Set the image feedback
+        val imageFeedback = mSyncDialog?.findViewById(R.id.sync_feedback_image) as ImageView
+        if (success) {
+            imageFeedback.setImageResource(R.drawable.ic_check_green_40dp)
+            responseView.setText(getString(R.string.executing_sync_complete))
+        }
+        else imageFeedback.setImageResource(R.drawable.ic_error_red_40dp)
+
+        val button = mSyncDialog?.findViewById(R.id.sync_verification_button) as androidx.appcompat.widget.AppCompatButton
+
+        // Show the updated views
+        progress.visibility = View.INVISIBLE
+        imageFeedback.visibility = View.VISIBLE
+        button.visibility = View.VISIBLE
+    }
+
+    /* Async Task Callbacks */
     override fun onPostExecute(response: String) {
-        Log.d(TAG, "onPostExecute: setting syncing to false and updating dialog")
-        syncing = false
+        Log.d(TAG, "onPostExecute: setting mSyncing to false and updating dialog")
+        mSyncing = false
         updateSyncDialog(response)
     }
 
     override fun onPreExecute() {
-        Log.d(TAG, "onPreExecute: setting syncing to true and show dialog")
-        syncing = true
+        Log.d(TAG, "onPreExecute: setting mSyncing to true and show dialog")
+        mSyncing = true
         showSyncDialog()
     }
 
@@ -76,89 +182,46 @@ class SettingsActivity : AppCompatActivity(), SettingsFragment.TaskCallbacks {
         // Do nothing
     }
 
-    fun createSyncDialog(){
-        Log.d(TAG, "creating sync dialog")
-        syncDialog = Dialog(this)
-        syncDialog?.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        // Set the dialog
-        syncDialog?.setCancelable(false)
-        syncDialog?.setContentView(R.layout.sync_dialog)
-
-        // Set up the button
-        val button = syncDialog?.findViewById(R.id.sync_verification_button) as androidx.appcompat.widget.AppCompatButton
-        button.setOnClickListener {
-            syncDialog?.dismiss()
-        }
+    /* Settings Fragment Callbacks */
+    override fun showVerifyProjectImportDialog() {
+        mShowingVerifySyncDialog = true
+        mVerifySyncDialog?.show()
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        Log.d(TAG, "saving instance state")
-        super.onSaveInstanceState(outState)
-        if (syncing == true) outState.putBoolean("syncing", true)
+    fun createVerifyProjectImportDialog(){
+        val context = this
+        val builder = AlertDialog.Builder(context)
+                .setTitle(R.string.warning)
+                .setMessage(R.string.database_sync_warning)
+                .setPositiveButton(R.string.ok) { _, _ ->
+                    Log.d("settings activity", "Launching database sync asynct task")
+                    // Execute the sync in the background
+                    (mTaskFragment as TaskFragment).executeDatabaseSync()
+                    mShowingVerifySyncDialog = false
+                }
+                .setNegativeButton(R.string.cancel){_,_ -> mShowingVerifySyncDialog = false}
+                .setIcon(R.drawable.ic_warning_black_24dp)
+
+        mVerifySyncDialog = builder.create()
     }
 
-    /*
-    override fun onPause() {
-        super.onPause()
-        if (syncDialog != null){
-            if (syncDialog?.isShowing!!)
-                syncDialog?.dismiss()
-        }
-    }
-    */
-
-    // Shows a dialog to give progress feedback on synchronization
-    fun showSyncDialog(){
-        Log.d(TAG, "showing sync dialog")
-        // Set the response
-        val responseView = syncDialog?.findViewById(R.id.sync_response) as TextView
-        responseView.setText(this.getString(R.string.executing_sync_notification))
-        val progress = syncDialog?.findViewById(R.id.sync_progress) as ProgressBar
-        /// Set the image feedback
-        val imageFeedback = syncDialog?.findViewById(R.id.sync_feedback_image) as ImageView
-        val button = syncDialog?.findViewById(R.id.sync_verification_button) as androidx.appcompat.widget.AppCompatButton
-
-        // Show the updated views
-        progress.visibility = View.VISIBLE
-        imageFeedback.visibility = View.INVISIBLE
-        button.visibility = View.GONE
-        // Show the dialog
-        syncDialog?.show()
+    override fun showFileModificationDialog() {
+        mShowingFileModDialog = true
+        mFileModDialog?.show()
     }
 
-    // Updates the dialog showing progress on synchronization
-    fun updateSyncDialog(response: String){
-        Log.d(TAG, "updating sync dialog")
-        val success = (response.equals(getString(R.string.valid_file_structure)))
-
-        // Set the response
-        val responseView = syncDialog?.findViewById(R.id.sync_response) as TextView
-        responseView.setText(response)
-
-        val progress = syncDialog?.findViewById(R.id.sync_progress) as ProgressBar
-
-        /// Set the image feedback
-        val imageFeedback = syncDialog?.findViewById(R.id.sync_feedback_image) as ImageView
-        if (success) {
-            imageFeedback.setImageResource(R.drawable.ic_check_green_40dp)
-            responseView.setText(getString(R.string.executing_sync_complete))
-        }
-        else imageFeedback.setImageResource(R.drawable.ic_error_red_40dp)
-
-        val button = syncDialog?.findViewById(R.id.sync_verification_button) as androidx.appcompat.widget.AppCompatButton
-
-        // Show the updated views
-        progress.visibility = View.INVISIBLE
-        imageFeedback.visibility = View.VISIBLE
-        button.visibility = View.VISIBLE
+    fun createManualFileModificationDialog() {
+        val builder = AlertDialog.Builder(this)
+                .setTitle(R.string.warning)
+                .setMessage(R.string.file_modification_information)
+                .setPositiveButton(R.string.ok) { _, _ -> mShowingFileModDialog = false}
+                .setIcon(R.drawable.ic_warning_black_24dp)
+        mFileModDialog = builder.create()
     }
 }
 
-class SettingsFragment : PreferenceFragmentCompat() {
+class TaskFragment: Fragment() {
     var mCallbacks: TaskCallbacks? = null
-    var prefs: SharedPreferences? = null
-    var prefListener: SharedPreferences.OnSharedPreferenceChangeListener? = null
-
 
     interface TaskCallbacks {
         fun onPreExecute()
@@ -175,6 +238,61 @@ class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         retainInstance = true
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        mCallbacks = null
+    }
+
+    fun executeDatabaseSync(){
+        DatabaseSyncTask().execute(inputParams(this.context!!))
+    }
+    // This async task validates the files are in correct format then synchronizes to the database
+    inner class DatabaseSyncTask : AsyncTask<inputParams, Void, String>() {
+
+        // Do in background validates then executes import
+        override fun doInBackground(vararg p0: inputParams): String {
+            val context = p0.get(0).context
+            val response = ProjectUtils.validateFileStructure(context)
+
+            // If modified files are valid go ahead notify user of background work and import
+            if (response.equals(context.getString(R.string.valid_file_structure))){
+                ProjectUtils.importProjects(context)
+            }
+
+            return response
+        }
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+            mCallbacks?.onPreExecute()
+        }
+
+        // On post execute updates sync dialog
+        override fun onPostExecute(result: String) {
+            super.onPostExecute(result)
+            mCallbacks?.onPostExecute(result)
+        }
+    }
+
+    // These classes used for passing references through the async task
+    class inputParams(val context: Context)
+}
+class SettingsFragment : PreferenceFragmentCompat() {
+    var mCallbacks: TaskCallbacks? = null
+    var prefs: SharedPreferences? = null
+    var prefListener: SharedPreferences.OnSharedPreferenceChangeListener? = null
+
+
+    interface TaskCallbacks {
+        fun showFileModificationDialog()
+        fun showVerifyProjectImportDialog()
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mCallbacks = context as TaskCallbacks
     }
 
     override fun onDetach() {
@@ -212,79 +330,18 @@ class SettingsFragment : PreferenceFragmentCompat() {
                 NotificationUtils.scheduleNotificationWorker(activity)
             }
 
-            // If the user enables manual file syncing give some info
+            // If the user enables manual file mSyncing give some info
             if (key.equals(getString(R.string.key_sync_allowed))){
                 val isSyncAllowed = prefs.getBoolean(key, false)
-                if (isSyncAllowed) showManualFileModificationDialog()
+                if (isSyncAllowed) mCallbacks?.showFileModificationDialog()
             }
         }
 
         // TODO (update) write unit test for file / database sync
         // Verify the user wants to sync files to the database
         syncPref?.setOnPreferenceClickListener{
-            verifyImportProjectsDialog()
+            mCallbacks?.showVerifyProjectImportDialog()
             true
         }
     }
-
-    // Verifies the user wishes to sync files to the database
-    fun verifyImportProjectsDialog(){
-        val fragment = this
-        val builder = AlertDialog.Builder(activity!!)
-                .setTitle(R.string.warning)
-                .setMessage(R.string.database_sync_warning)
-                .setPositiveButton(R.string.ok) { _, _ ->
-                    Log.d("settings activity", "Launching database sync asynct task")
-                    // Execute the sync in the background
-                    DatabaseSyncTask().execute(SettingsFragment.inputParams(fragment))
-                }
-                .setNegativeButton(R.string.cancel){_,_ -> }
-                .setIcon(R.drawable.ic_warning_black_24dp)
-
-        val dialog = builder.create()
-        dialog.show()
-    }
-
-    // Shows dialog that gives info on manual file modification
-    private fun showManualFileModificationDialog() {
-        val builder = AlertDialog.Builder(activity!!)
-                .setTitle(R.string.warning)
-                .setMessage(R.string.file_modification_information)
-                .setPositiveButton(R.string.ok) { _, _ ->  }
-                .setIcon(R.drawable.ic_warning_black_24dp)
-
-        val dialog = builder.create()
-        dialog.show()
-    }
-
-    // This async task validates the files are in correct format then synchronizes to the database
-    inner class DatabaseSyncTask : AsyncTask<inputParams, Void, String>() {
-
-        // Do in background validates then executes import
-        override fun doInBackground(vararg p0: inputParams): String {
-            val fragment = p0.get(0).fragment
-            val response = ProjectUtils.validateFileStructure(fragment.context)
-
-            // If modified files are valid go ahead notify user of background work and import
-            if (response.equals(fragment.getString(R.string.valid_file_structure))){
-                ProjectUtils.importProjects(fragment.context)
-            }
-
-            return response
-        }
-
-        override fun onPreExecute() {
-            super.onPreExecute()
-            mCallbacks?.onPreExecute()
-        }
-
-        // On post execute updates sync dialog
-        override fun onPostExecute(result: String) {
-            super.onPostExecute(result)
-            mCallbacks?.onPostExecute(result)
-        }
-    }
-
-    // These classes used for passing references through the async task
-    class inputParams(val fragment: PreferenceFragmentCompat)
 }
