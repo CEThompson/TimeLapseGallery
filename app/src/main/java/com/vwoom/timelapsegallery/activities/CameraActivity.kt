8d9d18f88a309273b292eea9fa4075a3dbf6867a
Christopher Thompson <ChristopherErikThompson@Gmail.com>
2019-10-22 09:27:37 -0700

    chore: add todo

--- app/src/main/java/com/vwoom/timelapsegallery/activities/CameraActivity.kt
+++ app/src/main/java/com/vwoom/timelapsegallery/activities/CameraActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.vwoom.timelapsegallery.R
 
+// TODO (update) create photo activity with camera x with a quick compare to previous photo function
 class CameraActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
