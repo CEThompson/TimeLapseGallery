0125e3c500f17fa5c70bb42d5be2e6c484107b66
Christopher Thompson <ChristopherErikThompson@Gmail.com>
2019-12-07 13:36:27 -0800

    refactor: implementing migration from db version 1 to 2

--- app/src/main/java/com/vwoom/timelapsegallery/database/TimeLapseDatabase.java
+++ app/src/main/java/com/vwoom/timelapsegallery/database/TimeLapseDatabase.java
@@ -2,9 +2,12 @@ package com.vwoom.timelapsegallery.database;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.vwoom.timelapsegallery.database.dao.CoverPhotoDao;
 import com.vwoom.timelapsegallery.database.dao.PhotoDao;
@@ -39,6 +42,7 @@ public abstract class TimeLapseDatabase extends RoomDatabase {
             synchronized (LOCK) {
                 sInstance = Room.databaseBuilder(context.getApplicationContext(),
                         TimeLapseDatabase.class, TimeLapseDatabase.DATABASE_NAME)
+                        .addMigrations(MIGRATION_1_2)
                         .build();
             }
         }
@@ -56,5 +60,46 @@ public abstract class TimeLapseDatabase extends RoomDatabase {
     public abstract CoverPhotoDao coverPhotoDao();
 
     public abstract ProjectScheduleDao projectScheduleDao();
+
+    static final Migration MIGRATION_1_2 = new Migration(1,2) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            // Create new tables
+            // (1) Cover Photo
+            // columns: project_id, photo_id
+            database.execSQL("CREATE TABLE cover_photo (project_id LONG, photo_id LONG, PRIMARY KEY(project_id))");
+
+            // (2) Project Schedule
+            // columns: project_id, schedule_time, interval_days
+            database.execSQL("CREATE TABLE project_schedule (project_id LONG, schedule_time LONG, interval_days INT, PRIMARY KEY(project_id))");
+
+            // Copy data into new tables
+            // (1) Project
+            // initial columns: id, name, thumbnail_url, schedule, schedule_next_submission, timestamp
+            // altered columns: id, project_name, cover_set_by_user
+
+            // Create new table
+            database.execSQL("CREATE TABLE project_new (id LONG, project_name TEXT, cover_set_by_user INT, PRIMARY KEY(id))");
+            // Copy the data
+            database.execSQL("INSERT INTO project_new (id, project_name) SELECT id, name FROM project");
+            // Remove old table
+            database.execSQL("DROP TABLE project");
+            // Change table name to old one
+            database.execSQL("ALTER TABLE project_new RENAME TO project");
+
+            // (2) Photo
+            // init: id, project_id, url, timestamp
+            // altered: id, project_id, timestamp
+
+            // (3) Project Tag
+            // init: id, tag_id, project_id
+            // altered: project_id, tag_id
+
+            // (4) Tag
+            // init: id, title
+            // altered: project_id, tag_id
+
+        }
+    };
 
 }
