60425bf0a2419629c5b4a29113263c4d15a58fe9
Christopher Thompson <ChristopherErikThompson@Gmail.com>
2019-12-09 09:52:56 -0800

    refactor: add parcelable view for project

--- app/src/main/java/com/vwoom/timelapsegallery/database/view/Project.kt
+++ app/src/main/java/com/vwoom/timelapsegallery/database/view/Project.kt
@@ -1,8 +1,10 @@
 package com.vwoom.timelapsegallery.database.view
 
+import android.os.Parcelable
 import androidx.room.DatabaseView
+import kotlinx.android.parcel.Parcelize
 
-/*
+
 @DatabaseView("SELECT " +
         "project.id AS project_id, " +
         "project.project_name AS project_name, " +
@@ -15,13 +17,13 @@ import androidx.room.DatabaseView
         "LEFT JOIN project_schedule ON project.id = project_schedule.project_id " +
         "LEFT JOIN cover_photo ON project.id = cover_photo.project_id " +
         "LEFT JOIN photo ON cover_photo.photo_id = photo.id")
+@Parcelize
 data class Project(
         val project_id: Long,
         val project_name: String?,
         val cover_set_by_user: Int,
-        val schedule_time: Long,
-        val interval_days: Int,
+        val schedule_time: Long?,
+        val interval_days: Int?,
         val cover_photo_id: Long,
         val cover_photo_timestamp: Long
-)
- */
+) : Parcelable
