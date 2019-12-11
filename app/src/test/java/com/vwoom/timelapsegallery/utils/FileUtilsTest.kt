0399b8eb94b7a4eb4e38dacfb9e87df994d9c4f7
Christopher Thompson <ChristopherErikThompson@Gmail.com>
2019-12-11 12:01:36 -0800

    refactor: implementing unit tests for file utils

--- app/src/test/java/com/vwoom/timelapsegallery/utils/FileUtilsTest.kt
+++ app/src/test/java/com/vwoom/timelapsegallery/utils/FileUtilsTest.kt
@@ -1,13 +1,40 @@
 package com.vwoom.timelapsegallery.utils
 
 
+import com.vwoom.timelapsegallery.database.entry.PhotoEntry
+import com.vwoom.timelapsegallery.database.entry.ProjectEntry
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import java.io.File
 
 class FileUtilsTest {
 
-    @Test
-    fun getPhotosInDirectory() {
+    @Rule @JvmField
+    val folder = TemporaryFolder()
 
+    /* Tests retrieving a list of photo entries from a project folder*/
+    @Test
+    fun getPhotosInDirectoryTest_() {
+        val projectName = "test project"
+        val projectEntry = ProjectEntry(1, projectName, 0)
+
+        // Create the folder for the project
+        val projectFolder = folder.newFolder("1_$projectName")
+
+        // Create files to test
+        var file = File(projectFolder, "1.jpg")
+        file.createNewFile()
+
+        file = File(projectFolder, "2.jpg")
+        file.createNewFile()
+
+        val listOfPhotoEntries = FileUtils.getPhotosInDirectory(projectFolder, projectEntry)
+        assert(listOfPhotoEntries.equals(
+                listOf(
+                PhotoEntry(1,1,1),
+                PhotoEntry(2,1,2))
+        ))
     }
 
     @Test
@@ -41,29 +68,4 @@ class FileUtilsTest {
     @Test
     fun pathContainsReservedCharacter() {
     }
-
-    @get:Test
-    val photoUrl: Unit
-        get() {
-        }
-
-    @get:Test
-    val photoUrl1: Unit
-        get() {
-        }
-
-    @get:Test
-    val photoUrl2: Unit
-        get() {
-        }
-
-    @get:Test
-    val photoFileName: Unit
-        get() {
-        }
-
-    @get:Test
-    val photoFileName1: Unit
-        get() {
-        }
 }
