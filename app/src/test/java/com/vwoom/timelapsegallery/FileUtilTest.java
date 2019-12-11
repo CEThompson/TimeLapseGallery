7ba4bae2697d040aca6f6b376831e2815939c9cd
Christopher Thompson <ChristopherErikThompson@Gmail.com>
2019-12-10 16:52:38 -0800

    refactor: adding unit tests

---
+++ app/src/test/java/com/vwoom/timelapsegallery/FileUtilTest.java
@@ -0,0 +1,18 @@
+package com.vwoom.timelapsegallery;
+
+public class FileUtilTest {
+
+
+    /*
+    private static File createImageFile(Context context, ProjectEntry currentProject, long timestamp) {
+        // Create an image file name from the current timestamp
+        String imageFileName = timestamp + ".jpg";
+        File projectDir = getProjectFolder(context, currentProject);
+
+        if (!projectDir.exists()) projectDir.mkdirs();
+
+        return new File(projectDir, imageFileName);
+    }
+     */
+
+}
