1b31223b691ca33a4eab20359ba8b5d396fda7d6
Christopher Thompson <ChristopherErikThompson@Gmail.com>
2019-12-11 09:40:17 -0800

    refactor: adding tests

---
+++ app/src/test/java/com/vwoom/timelapsegallery/utils/FileUtilsTest.kt
@@ -0,0 +1,69 @@
+package com.vwoom.timelapsegallery.utils
+
+
+import org.junit.Test
+
+class FileUtilsTest {
+
+    @Test
+    fun getPhotosInDirectory() {
+
+    }
+
+    @Test
+    fun createTemporaryImageFile() {
+    }
+
+    @Test
+    fun createFinalFileFromTemp() {
+    }
+
+    @Test
+    fun renameProject() {
+    }
+
+    @Test
+    fun deleteTempFiles() {
+    }
+
+    @Test
+    fun deleteProject() {
+    }
+
+    @Test
+    fun deletePhoto() {
+    }
+
+    @Test
+    fun deletePhoto1() {
+    }
+
+    @Test
+    fun pathContainsReservedCharacter() {
+    }
+
+    @get:Test
+    val photoUrl: Unit
+        get() {
+        }
+
+    @get:Test
+    val photoUrl1: Unit
+        get() {
+        }
+
+    @get:Test
+    val photoUrl2: Unit
+        get() {
+        }
+
+    @get:Test
+    val photoFileName: Unit
+        get() {
+        }
+
+    @get:Test
+    val photoFileName1: Unit
+        get() {
+        }
+}
