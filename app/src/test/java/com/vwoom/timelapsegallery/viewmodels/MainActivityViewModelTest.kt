dc8d9dfe8b542b1b811d95d9249cd1f584ef5a79
Christopher Thompson <ChristopherErikThompson@Gmail.com>
2019-12-12 11:31:47 -0800

    refactor: add dependencies to test viewmodels

---
+++ app/src/test/java/com/vwoom/timelapsegallery/viewmodels/MainActivityViewModelTest.kt
@@ -0,0 +1,19 @@
+package com.vwoom.timelapsegallery.viewmodels
+
+import org.junit.Test
+
+class MainActivityViewModelTest {
+
+    @Test
+    fun getProjects() {
+        // Given - some projects in database
+        val mainActivityViewModel = MainActivityViewModel()
+
+        // When calling getProjects()
+
+
+        // Then a list of the projects is returned
+
+
+    }
+}
