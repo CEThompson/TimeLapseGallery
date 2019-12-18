bccdf695e1e178932dda49b3468a1235bcc7acb7
Christopher Thompson <ChristopherErikThompson@Gmail.com>
2019-12-18 13:26:23 -0800

    refactor: remove autogenerated fragment methods

--- app/src/main/java/com/vwoom/timelapsegallery/activities/GalleryFragment.kt
+++ app/src/main/java/com/vwoom/timelapsegallery/activities/GalleryFragment.kt
@@ -30,8 +30,6 @@ import com.vwoom.timelapsegallery.viewmodels.MainActivityViewModel
 
 
 class GalleryFragment : Fragment(), ProjectsAdapter.ProjectsAdapterOnClickHandler {
-    // TODO: Rename and change types of parameters
-    private var listener: OnFragmentInteractionListener? = null
 
     @BindView(R.id.add_project_FAB)
     var mNewProjectFab: FloatingActionButton? = null
@@ -73,60 +71,6 @@ class GalleryFragment : Fragment(), ProjectsAdapter.ProjectsAdapterOnClickHandle
         return inflater.inflate(R.layout.fragment_gallery, container, false)
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        listener?.onFragmentInteraction(uri)
-    }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            listener = context
-        } else {
-            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
-        }
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments]
-     * (http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GalleryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                GalleryFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
-    }
 
     /* Sets up view models */
     private fun setupViewModel() {
