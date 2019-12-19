1276ec5a99304b2d3cbd154d7700a944ff144fcb
Christopher Thompson <ChristopherErikThompson@Gmail.com>
2019-12-18 16:19:54 -0800

    refactor: change view model to gallery instead of main activity

--- app/src/main/java/com/vwoom/timelapsegallery/viewmodels/GalleryViewModel.java
+++ app/src/main/java/com/vwoom/timelapsegallery/viewmodels/GalleryViewModel.java
@@ -1,20 +1,28 @@
-package com.vwoom.timelapsegallery.viewmodels
+package com.vwoom.timelapsegallery.viewmodels;
 
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import com.vwoom.timelapsegallery.data.TimeLapseDatabase.Companion.getInstance
-import com.vwoom.timelapsegallery.data.view.Project
+import android.app.Application;
 
-class GalleryViewModel(application: Application?) : AndroidViewModel(application!!) {
-    val projects: LiveData<List<Project>>
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
 
-    companion object {
-        private val TAG = GalleryViewModel::class.java.simpleName
+import com.vwoom.timelapsegallery.data.TimeLapseDatabase;
+import com.vwoom.timelapsegallery.data.view.Project;
+
+import java.util.List;
+
+public class GalleryViewModel extends AndroidViewModel {
+
+    private static final String TAG = GalleryViewModel.class.getSimpleName();
+
+    private LiveData<List<Project>> projects;
+
+    public GalleryViewModel(Application application) {
+        super(application);
+        TimeLapseDatabase database = TimeLapseDatabase.getInstance(this.getApplication());
+        projects = database.projectDao().loadProjectViews();
     }
 
-    init {
-        val database = getInstance(getApplication())
-        projects = database.projectDao().loadProjectViews()
+    public LiveData<List<Project>> getProjects() {
+        return projects;
     }
 }
