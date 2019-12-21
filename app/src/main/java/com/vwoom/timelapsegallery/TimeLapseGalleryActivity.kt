25f183ebcdab4498933f1632213b00dc3e4cd98d
Christopher Thompson <ChristopherErikThompson@Gmail.com>
2019-12-21 09:36:43 -0800

    chore: remove unused imports

--- app/src/main/java/com/vwoom/timelapsegallery/TimeLapseGalleryActivity.kt
+++ app/src/main/java/com/vwoom/timelapsegallery/TimeLapseGalleryActivity.kt
@@ -1,21 +1,12 @@
 package com.vwoom.timelapsegallery
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import butterknife.ButterKnife
 import com.google.android.gms.ads.MobileAds
 import com.google.android.gms.ads.initialization.InitializationStatus
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener
-import com.vwoom.timelapsegallery.R
 import com.vwoom.timelapsegallery.utils.FileUtils
-
-// TODO refactor app to use one activity with multiple fragments paradigm
-// TODO convert activities
 
 class TimeLapseGalleryActivity : AppCompatActivity() {
 
