9ac40ddfb965752d818518d3dfdd77ae80b6bbd9
Christopher Thompson <ChristopherErikThompson@Gmail.com>
2019-12-23 11:21:07 -0800

    refactor: add view model for settings fragment

---
+++ app/src/main/java/com/vwoom/timelapsegallery/settings/SettingsViewModel.kt
@@ -0,0 +1,9 @@
+package com.vwoom.timelapsegallery.settings
+
+import androidx.lifecycle.ViewModel
+
+class SettingsViewModel : ViewModel() {
+
+    // TODO handle database sync
+    // TODO handle dialog state
+}
