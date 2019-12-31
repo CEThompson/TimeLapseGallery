9e9a1563744bc5126de3560b00dc086421b67d31
Erik <vwoomdev@gmail.com>
2019-12-30 20:31:36 -0800

    chore: remove unused imports

--- app/src/main/java/com/vwoom/timelapsegallery/details/DetailsAdapter.kt
+++ app/src/main/java/com/vwoom/timelapsegallery/details/DetailsAdapter.kt
@@ -5,18 +5,13 @@ import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.vwoom.timelapsegallery.R
 import com.vwoom.timelapsegallery.details.DetailsAdapter.DetailsAdapterViewHolder
 import com.vwoom.timelapsegallery.data.entry.PhotoEntry
 import com.vwoom.timelapsegallery.data.view.Project
 import com.vwoom.timelapsegallery.databinding.DetailRecyclerviewItemBinding
-import com.vwoom.timelapsegallery.databinding.GalleryRecyclerviewItemBinding
 import com.vwoom.timelapsegallery.utils.FileUtils
-import kotlinx.android.synthetic.main.detail_recyclerview_item.view.*
 import java.io.File
 
 class DetailsAdapter(private val mClickHandler: DetailsAdapterOnClickHandler, context: Context) : RecyclerView.Adapter<DetailsAdapterViewHolder>() {
