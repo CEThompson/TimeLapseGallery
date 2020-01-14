0b0b8560d6b44bff3624aef7b1306d404ea0bd01
Erik <vwoomdev@gmail.com>
2020-01-14 10:22:40 -0800

    refactor: convert app executor usage to coroutine, remove app executors

--- app/src/main/java/com/vwoom/timelapsegallery/widget/UpdateWidgetService.kt
+++ app/src/main/java/com/vwoom/timelapsegallery/widget/UpdateWidgetService.kt
@@ -6,12 +6,18 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.util.Log
-import com.vwoom.timelapsegallery.data.AppExecutors.Companion.instance
 import com.vwoom.timelapsegallery.data.TimeLapseDatabase.Companion.getInstance
-import com.vwoom.timelapsegallery.widget.WidgetProvider
 import com.vwoom.timelapsegallery.widget.WidgetProvider.Companion.updateWidgets
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
-class UpdateWidgetService : IntentService("UpdateWidgetService") {
+class UpdateWidgetService : IntentService("UpdateWidgetService"), CoroutineScope {
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.IO
+
     override fun onHandleIntent(intent: Intent?) {
         if (intent != null) {
             val action = intent.action
@@ -22,21 +28,23 @@ class UpdateWidgetService : IntentService("UpdateWidgetService") {
     }
 
     private fun updateWidgets() {
-        instance!!.diskIO().execute {
-            // Retrieve the database
-            val timeLapseDatabase = getInstance(this)
-            // Get the list of all scheduled projects from the database
-            val allScheduledProjects = timeLapseDatabase.projectDao().loadAllScheduledProjects()
-            // Get the list of projects scheduled for today
-            val projectsScheduledForToday = timeLapseDatabase.projectDao().loadAllScheduledProjects()
-            // Update the widgets
-            val appWidgetManager = AppWidgetManager.getInstance(this)
-            val appWidgetIds = appWidgetManager.getAppWidgetIds(ComponentName(this, WidgetProvider::class.java))
-            updateWidgets(
-                    this,
-                    appWidgetManager,
-                    appWidgetIds,
-                    projectsScheduledForToday) // Send the list of todays projects
+        val context = this
+        launch {
+                // Retrieve the database
+                val timeLapseDatabase = getInstance(context)
+                // Get the list of all scheduled projects from the database
+                val allScheduledProjects = timeLapseDatabase.projectDao().loadAllScheduledProjects()
+                // Get the list of projects scheduled for today
+                val projectsScheduledForToday = timeLapseDatabase.projectDao().loadAllScheduledProjects()
+                // Update the widgets
+                val appWidgetManager = AppWidgetManager.getInstance(context)
+                val appWidgetIds = appWidgetManager.getAppWidgetIds(ComponentName(context, WidgetProvider::class.java))
+                updateWidgets(
+                        context,
+                        appWidgetManager,
+                        appWidgetIds,
+                        projectsScheduledForToday) // Send the list of todays projects
+
         }
     }
 
