3e6ed71980b2ad5929ac24467a01a9d3c0b0b2f2
Erik <vwoomdev@gmail.com>
2020-01-21 13:04:17 -0800

    refactor: setting up photo utils test

---
+++ app/src/test/java/com/vwoom/timelapsegallery/utils/PhotoUtilsTest.kt
@@ -0,0 +1,39 @@
+package com.vwoom.timelapsegallery.utils
+
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+
+// TODO convert to instrumented test?
+class PhotoUtilsTest {
+
+    @Rule
+    @JvmField
+    val testFolder = TemporaryFolder()
+
+    @Test
+    fun getAspectRatioFromImagePath() {
+
+    }
+
+    @Test
+    fun getOrientationFromImagePath() {
+    }
+
+    @Test
+    fun rotateBitmap() {
+    }
+
+    @Test
+    fun isLandscape() {
+    }
+
+    @Test
+    fun decodeSampledBitmapFromPath() {
+    }
+
+    @Test
+    fun calculateInSampleSize() {
+    }
+
+}
