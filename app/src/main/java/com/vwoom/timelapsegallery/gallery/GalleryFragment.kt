a8814562c040857decf684b74dad1dc4592708f7
Erik <vwoomdev@gmail.com>
2020-01-23 14:14:46 -0800

    refactor: handle tags as job

--- app/src/main/java/com/vwoom/timelapsegallery/gallery/GalleryFragment.kt
+++ app/src/main/java/com/vwoom/timelapsegallery/gallery/GalleryFragment.kt
@@ -1,13 +1,11 @@
 package com.vwoom.timelapsegallery.gallery
 
 import android.app.Dialog
-import android.content.res.ColorStateList
 import android.content.res.Configuration
 import android.os.Bundle
 import android.view.*
 import android.widget.CheckBox
 import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -16,7 +14,6 @@ import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.google.android.flexbox.FlexboxLayout
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.vwoom.timelapsegallery.R
 import com.vwoom.timelapsegallery.TimeLapseGalleryActivity
 import com.vwoom.timelapsegallery.data.entry.TagEntry
@@ -24,6 +21,7 @@ import com.vwoom.timelapsegallery.data.view.Project
 import com.vwoom.timelapsegallery.databinding.FragmentGalleryBinding
 import com.vwoom.timelapsegallery.databinding.GalleryRecyclerviewItemBinding
 import com.vwoom.timelapsegallery.utils.InjectorUtils
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 class GalleryFragment : Fragment(), GalleryAdapter.GalleryAdapterOnClickHandler {
@@ -37,6 +35,8 @@ class GalleryFragment : Fragment(), GalleryAdapter.GalleryAdapterOnClickHandler 
 
     private lateinit var mBinding: FragmentGalleryBinding
 
+    private var tagJob: Job? = null
+
     private val mGalleryViewModel: GalleryViewModel by viewModels {
         InjectorUtils.provideGalleryViewModelFactory(requireActivity())
     }
@@ -49,6 +49,11 @@ class GalleryFragment : Fragment(), GalleryAdapter.GalleryAdapterOnClickHandler 
     override fun onPause() {
         super.onPause()
         mFilterDialog?.dismiss()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        tagJob?.cancel()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -133,8 +138,9 @@ class GalleryFragment : Fragment(), GalleryAdapter.GalleryAdapterOnClickHandler 
     }
 
     private fun updateSearchFilter(){
+        tagJob?.cancel()
         mGalleryViewModel.setFilter(mFilterTags)
-        mGalleryViewModel.viewModelScope.launch {
+        tagJob = mGalleryViewModel.viewModelScope.launch {
             val filteredProjects = mGalleryViewModel.filterProjects(mProjects!!)
             mGalleryAdapter?.setProjectData(filteredProjects)
         }
