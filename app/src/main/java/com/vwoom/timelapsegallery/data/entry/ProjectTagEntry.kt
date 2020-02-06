c614dc72f2580b7fcc8d16c52b3bd27fb3463b77
Erik <vwoomdev@gmail.com>
2020-02-06 13:52:22 -0800

    style: reformat code

--- app/src/main/java/com/vwoom/timelapsegallery/data/entry/ProjectTagEntry.kt
+++ app/src/main/java/com/vwoom/timelapsegallery/data/entry/ProjectTagEntry.kt
@@ -8,14 +8,16 @@ import androidx.room.PrimaryKey
 // TODO verify cascade deletion works appropriately
 // TODO set index for columns
 @Entity(tableName = "project_tag",
-        foreignKeys = [ForeignKey(entity = ProjectEntry::class,
+        foreignKeys = [
+        ForeignKey(entity = ProjectEntry::class,
                 parentColumns = ["id"],
                 childColumns = ["project_id"],
                 onDelete = ForeignKey.CASCADE),
-            ForeignKey(entity = TagEntry::class,
-                    parentColumns = ["id"],
-                    childColumns = ["tag_id"],
-                    onDelete = ForeignKey.CASCADE)])
+        ForeignKey(entity = TagEntry::class,
+                parentColumns = ["id"],
+                childColumns = ["tag_id"],
+                onDelete = ForeignKey.CASCADE)
+        ])
 data class ProjectTagEntry(var project_id: Long, var tag_id: Long){
         @PrimaryKey(autoGenerate = true)
         var id: Long = 0
