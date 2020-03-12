b9e22e52e3a67038880d7cc8490ccc555062b166
Erik <vwoomdev@gmail.com>
2020-03-11 17:36:51 -0700

    refactor: set detail adapter to null in on destroy to prevent memory leak

--- app/src/main/java/com/vwoom/timelapsegallery/detail/DetailFragment.kt
+++ app/src/main/java/com/vwoom/timelapsegallery/detail/DetailFragment.kt
@@ -168,6 +168,7 @@ class DetailFragment : Fragment(), DetailAdapter.DetailAdapterOnClickHandler {
         mScheduleDialog = null
         mInfoDialog = null
         mFullscreenImageDialog = null
+        mDetailAdapter = null
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
