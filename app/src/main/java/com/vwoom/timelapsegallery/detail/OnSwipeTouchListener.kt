d08e5fd61630b8ce0da65d79dc3ae3580e586c5a
Erik <vwoomdev@gmail.com>
2020-04-02 11:56:00 -0700

    style: reformat code

--- app/src/main/java/com/vwoom/timelapsegallery/detail/OnSwipeTouchListener.kt
+++ app/src/main/java/com/vwoom/timelapsegallery/detail/OnSwipeTouchListener.kt
@@ -6,14 +6,8 @@ import android.view.MotionEvent
 import android.view.View
 import kotlin.math.absoluteValue
 
-// Changes photo on swipe
-class OnSwipeTouchListener(ctx: Context?, private val onSwipeRight: ()-> Unit, private val onSwipeLeft: ()-> Unit) : View.OnTouchListener {
+class OnSwipeTouchListener(ctx: Context?, private val onSwipeRight: () -> Unit, private val onSwipeLeft: () -> Unit) : View.OnTouchListener {
     private val gestureDetector: GestureDetector
-
-    //interface SwipeHandler {
-    //    fun onSwipeLeft()
-    //    fun onSwipeRight()
-    //}
 
     @Suppress("ClickableViewAccessibility")
     override fun onTouch(v: View, event: MotionEvent): Boolean {
@@ -45,9 +39,7 @@ class OnSwipeTouchListener(ctx: Context?, private val onSwipeRight: ()-> Unit, p
     }
 
     fun onSwipeRight() = onSwipeRight.invoke()
-            //detailViewModel.previousPhoto()
     fun onSwipeLeft() = onSwipeLeft.invoke()
-                //detailViewModel.nextPhoto()
 
     init {
         gestureDetector = GestureDetector(ctx, GestureListener())
