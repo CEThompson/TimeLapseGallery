194e7c675f9c17828f63a70722fa8789f0ddb918
Erik <vwoomdev@gmail.com>
2020-04-01 18:44:59 -0700

    refactor: remove unnecessary non-null assertion

--- app/src/main/java/com/vwoom/timelapsegallery/detail/DetailAdapter.kt
+++ app/src/main/java/com/vwoom/timelapsegallery/detail/DetailAdapter.kt
@@ -5,10 +5,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.vwoom.timelapsegallery.detail.DetailAdapter.DetailAdapterViewHolder
 import com.vwoom.timelapsegallery.data.entry.PhotoEntry
 import com.vwoom.timelapsegallery.data.view.Project
 import com.vwoom.timelapsegallery.databinding.DetailRecyclerviewItemBinding
+import com.vwoom.timelapsegallery.detail.DetailAdapter.DetailAdapterViewHolder
 import com.vwoom.timelapsegallery.utils.FileUtils
 import com.vwoom.timelapsegallery.utils.ProjectUtils.getProjectEntryFromProjectView
 import java.io.File
@@ -23,14 +23,12 @@ class DetailAdapter(private val mClickHandler: DetailAdapterOnClickHandler, val 
     }
 
     inner class DetailAdapterViewHolder(var binding: DetailRecyclerviewItemBinding) : RecyclerView.ViewHolder(binding.root), View.OnClickListener {
-
         override fun onClick(view: View) {
             val adapterPosition = adapterPosition
             val clickedPhoto = mPhotos!![adapterPosition]
             mCurrentPhoto = clickedPhoto
             mClickHandler.onClick(clickedPhoto)
         }
-
         init {
             binding.root.setOnClickListener(this)
         }
@@ -58,7 +56,7 @@ class DetailAdapter(private val mClickHandler: DetailAdapterOnClickHandler, val 
                 .load(f)
                 .centerCrop()
                 .into(binding.detailThumbnail)
-        if (mPhotos!![position] === mCurrentPhoto) {
+        if (mPhotos?.get(position) === mCurrentPhoto) {
             binding.selectionIndicator.visibility = View.VISIBLE
         } else {
             binding.selectionIndicator.visibility = View.INVISIBLE
