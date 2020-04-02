4a46c1a7cb6b4ce220bf56760722e7bdb5a89fc9
Erik <vwoomdev@gmail.com>
2020-04-01 18:36:59 -0700

    refactor: wrap file write in io context

--- app/src/main/java/com/vwoom/timelapsegallery/data/repository/ProjectScheduleRepository.kt
+++ app/src/main/java/com/vwoom/timelapsegallery/data/repository/ProjectScheduleRepository.kt
@@ -4,12 +4,15 @@ import com.vwoom.timelapsegallery.data.dao.ProjectScheduleDao
 import com.vwoom.timelapsegallery.data.entry.ProjectScheduleEntry
 import com.vwoom.timelapsegallery.data.view.Project
 import com.vwoom.timelapsegallery.utils.FileUtils
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.io.File
 
 class ProjectScheduleRepository private constructor(private val projectScheduleDao: ProjectScheduleDao) {
 
     companion object {
-        @Volatile private var instance: ProjectScheduleRepository? = null
+        @Volatile
+        private var instance: ProjectScheduleRepository? = null
 
         fun getInstance(projectScheduleDao: ProjectScheduleDao) =
                 instance ?: synchronized(this) {
@@ -17,11 +20,17 @@ class ProjectScheduleRepository private constructor(private val projectScheduleD
                 }
     }
 
-    suspend fun setProjectSchedule(externalFilesDir: File, project: Project, projectScheduleEntry: ProjectScheduleEntry) {
+    suspend fun setProjectSchedule(
+            externalFilesDir: File,
+            project: Project,
+            projectScheduleEntry: ProjectScheduleEntry) {
+
         // Write the project schedule to the database
         projectScheduleDao.insertProjectSchedule(projectScheduleEntry)
 
-        // Handle the file representation of the schedule
-        FileUtils.writeProjectScheduleFile(externalFilesDir, project.project_id, projectScheduleEntry)
+        withContext(Dispatchers.IO) {
+            // Handle the file representation of the schedule
+            FileUtils.writeProjectScheduleFile(externalFilesDir, project.project_id, projectScheduleEntry)
+        }
     }
 }
