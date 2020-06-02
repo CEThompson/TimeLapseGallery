2a519a2b399f428c4ba99f983e97236906c5f2dd
Erik <vwoomdev@gmail.com>
2020-06-02 16:35:21 -0700

    style: add note for application delayed launch

--- app/src/main/java/com/vwoom/timelapsegallery/TimeLapseGalleryApplication.kt
+++ app/src/main/java/com/vwoom/timelapsegallery/TimeLapseGalleryApplication.kt
@@ -4,8 +4,20 @@ import android.app.Application
 import androidx.camera.camera2.Camera2Config
 import androidx.camera.core.CameraXConfig
 
-class TimeLapseGalleryApplication : Application(),  CameraXConfig.Provider {
+class TimeLapseGalleryApplication : Application(), CameraXConfig.Provider {
     override fun getCameraXConfig(): CameraXConfig {
         return Camera2Config.defaultConfig()
     }
+
+    // Use this block for delayed launch
+    /*override fun onCreate() {
+        super.onCreate()
+        delayedInit()
+    }
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
+    private fun delayedInit(){
+        applicationScope.launch {
+            // Late init application stuff here in order to prevent launch delay
+        }
+    }*/
 }
