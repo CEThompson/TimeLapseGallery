c05110edf28e23edf9d6be4d4a87aaa24b90822f
Erik <vwoomdev@gmail.com>
2020-06-12 21:10:10 -0700

    chore: add todo to add setting for gif display

--- app/src/main/java/com/vwoom/timelapsegallery/settings/SettingsFragment.kt
+++ app/src/main/java/com/vwoom/timelapsegallery/settings/SettingsFragment.kt
@@ -31,6 +31,8 @@ import com.vwoom.timelapsegallery.utils.RESERVED_CHARACTERS
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.async
 
+// TODO:  Create setting to disable gif display in the gallery
+
 // TODO: (update 1.2) reevaluate shared preference options and content
 // TODO: (update 1.2) re-evaluate free and paid variants
 // TODO: (update 1.2) re-evaluate  analytics metrics
