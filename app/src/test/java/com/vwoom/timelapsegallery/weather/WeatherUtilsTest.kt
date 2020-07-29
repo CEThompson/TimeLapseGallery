93672807312d6be25ec5b2f921ded08bfb63ba94
Erik <vwoomdev@gmail.com>
2020-07-29 15:56:25 -0700

    chore: add todo to test weather utils

---
+++ app/src/test/java/com/vwoom/timelapsegallery/weather/WeatherUtilsTest.kt
@@ -0,0 +1,17 @@
+package com.vwoom.timelapsegallery.weather
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+// TODO test weather utils
+class WeatherUtilsTest {
+
+    @Test
+    fun getWeatherIcon() {
+    }
+
+    @Test
+    fun getTimestampFromPeriod() {
+    }
+}
