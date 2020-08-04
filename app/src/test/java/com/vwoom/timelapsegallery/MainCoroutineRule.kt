9463ae799499ff31c8a31e0be39864401014601a
Erik <vwoomdev@gmail.com>
2020-08-04 14:49:05 -0700

    chore: modify coroutine rule to advanced android udacity example

--- app/src/test/java/com/vwoom/timelapsegallery/MainCoroutineRule.kt
+++ app/src/test/java/com/vwoom/timelapsegallery/MainCoroutineRule.kt
@@ -3,6 +3,7 @@ package com.vwoom.timelapsegallery
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.TestCoroutineScope
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
@@ -10,7 +11,9 @@ import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 import kotlin.coroutines.ContinuationInterceptor
 
-@ExperimentalCoroutinesApi
+
+// This block from unknown googled source
+/*@ExperimentalCoroutinesApi
 class MainCoroutineRule : TestWatcher(), TestCoroutineScope by TestCoroutineScope() {
 
     override fun starting(description: Description?) {
@@ -20,6 +23,24 @@ class MainCoroutineRule : TestWatcher(), TestCoroutineScope by TestCoroutineScop
 
     override fun finished(description: Description?) {
         super.finished(description)
+        Dispatchers.resetMain()
+    }
+}*/
+
+
+// This block from advanced android course
+@ExperimentalCoroutinesApi
+class MainCoroutineRule(private val dispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher())
+    : TestWatcher(), TestCoroutineScope by TestCoroutineScope(dispatcher) {
+
+    override fun starting(description: Description?) {
+        super.starting(description)
+        Dispatchers.setMain(dispatcher)
+    }
+
+    override fun finished(description: Description?) {
+        super.finished(description)
+        cleanupTestCoroutines()
         Dispatchers.resetMain()
     }
 }
