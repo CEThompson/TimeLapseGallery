2b72df73188254ac18aa2bc885926f9abf80ac81
Erik <vwoomdev@gmail.com>
2020-08-11 10:17:41 -0700

    chore: convert to timber

--- app/src/main/java/com/vwoom/timelapsegallery/notification/NotificationAlarm.kt
+++ app/src/main/java/com/vwoom/timelapsegallery/notification/NotificationAlarm.kt
@@ -6,7 +6,6 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.PowerManager
-import android.util.Log
 import com.vwoom.timelapsegallery.BuildConfig
 import com.vwoom.timelapsegallery.data.repository.ProjectRepository
 import com.vwoom.timelapsegallery.notification.NotificationUtils.clearPreviousNotifications
@@ -17,6 +16,7 @@ import com.vwoom.timelapsegallery.utils.ProjectUtils
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import java.util.*
 
 class NotificationAlarm : BroadcastReceiver() {
@@ -25,7 +25,7 @@ class NotificationAlarm : BroadcastReceiver() {
 
     // This receives the pending intent broadcast from the notification alarm and creates a notification
     override fun onReceive(context: Context, intent: Intent) {
-        Log.d(TAG, "Notification Tracker: Receiving intent to create notification")
+        Timber.d("Notification Tracker: Receiving intent to create notification")
 
         // Set up the wake lock
         val pm = context.getSystemService(Context.POWER_SERVICE) as PowerManager
@@ -35,8 +35,8 @@ class NotificationAlarm : BroadcastReceiver() {
 
         // Get the request code which is the day of the year
         val requestCode = intent.getIntExtra(REQUEST_CODE, 0)
-        Log.d(TAG, "Notification Tracker: Request code is $requestCode")
-        Log.d(TAG, "Notification Tracker: Action type is " + intent.action)
+        Timber.d("Notification Tracker: Request code is $requestCode")
+        Timber.d("Notification Tracker: Action type is ${intent.action}")
         // Clear any previous notifications that are unused
         clearPreviousNotifications(context)
 
@@ -48,8 +48,8 @@ class NotificationAlarm : BroadcastReceiver() {
                 ProjectUtils.isProjectDueToday(it)
             }
 
-            Log.d(TAG, "there are ${dueProjects.size} projects due")
-            Log.d(TAG, "due projects is not empty ${dueProjects.isNotEmpty()}")
+            Timber.d("there are ${dueProjects.size} projects due")
+            Timber.d("due projects is not empty ${dueProjects.isNotEmpty()}")
             // If there are projects due today, send the notification
             if (dueProjects.isNotEmpty()) notifyUserOfScheduledProjects(context, requestCode)
             // Release the wake lock
@@ -59,7 +59,7 @@ class NotificationAlarm : BroadcastReceiver() {
 
     // Sets an alarm for tomorrow at the shared preference time
     fun setAlarmForTomorrow(context: Context) {
-        Log.d(TAG, "Notification Tracker: Setting alarm for tomorrow")
+        Timber.d("Notification Tracker: Setting alarm for tomorrow")
         // Get the day of the year and set it to the request code
         val calendar = Calendar.getInstance()
         calendar.timeInMillis = System.currentTimeMillis()
@@ -67,7 +67,7 @@ class NotificationAlarm : BroadcastReceiver() {
         val requestCode = today + 1
 
         // Create the pending intent to broadcast on that day
-        Log.d(TAG, "Notification Tracker: Request code / day of year is $requestCode")
+        Timber.d("Notification Tracker: Request code / day of year is $requestCode")
         val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
         val intent = Intent(context, NotificationAlarm::class.java)
         intent.putExtra(REQUEST_CODE, requestCode)
@@ -84,7 +84,7 @@ class NotificationAlarm : BroadcastReceiver() {
 
     // Helper function that Cancels an alarm by the day of year
     private fun cancelAlarmByDay(context: Context, dayOfYear: Int) {
-        Log.d(TAG, "Notification Tracker: Canceling alarm for day $dayOfYear")
+        Timber.d("Notification Tracker: Canceling alarm for day $dayOfYear")
 
         // Identify the alarm
         val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
@@ -100,7 +100,7 @@ class NotificationAlarm : BroadcastReceiver() {
     // Cancels any alarms set for today or tomorrow
     // This should be any alarms set
     fun cancelAlarms(context: Context) {
-        Log.d(TAG, "Notification Tracker: Canceling alarms for today and tomorrow")
+        Timber.d("Notification Tracker: Canceling alarms for today and tomorrow")
         val calendar = Calendar.getInstance()
         calendar.timeInMillis = System.currentTimeMillis()
         val today = calendar[Calendar.DAY_OF_YEAR]
